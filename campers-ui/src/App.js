2e79e1973b64a46792ab7f0d48a50334111e7f65
shannonjohnstone <shannon@shannonjohnstone.com.au>
2019-03-02 13:56:39 +1100

    update mock data

--- campers-ui/src/App.js
+++ campers-ui/src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import CampersContainer from './containers/CampersContainer';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import CampersContainer from './containers/campers/CampersContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="">
-        <header className="">
-          <p>Campers Leader Board</p>
-        </header>
-        <CampersContainer />
-      </div>
+      <Container>
+        <Row>
+          {/* <Col lg={6}> */}
+          <Col>
+            <header className="">
+              <p>Campers Leader Board</p>
+            </header>
+            <CampersContainer />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
